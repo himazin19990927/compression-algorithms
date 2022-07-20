57423c7db1c667964a106c2cfa8a6182cda0c93f
Taiki Harada <haradataiki19990927@gmail.com>
2022-07-20 02:14:47 +0000

    Add run-length

---
+++ CompressSRC/chapter1/list1-1/main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 102400
+
+typedef unsigned char byte;
+
+void encode_data(FILE *fp, byte *datas, int data_size);
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp_input = fopen(argv[1], "rb");
+    if (fp_input == NULL) {
+        fprintf(stderr, "Input File cannot open\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // ファイルデータをバッファに取り込む
+    int data_size = 0;
+    int c;
+    unsigned char buffer[BUFFER_SIZE];
+    while (data_size < BUFFER_SIZE && (c = fgetc(fp_input)) != EOF) {
+        buffer[data_size] = c;
+        data_size++;
+    }
+    fclose(fp_input);
+
+    // 圧縮処理をする
+    FILE *fp_output = fopen(argv[2], "wb");
+    if (fp_output == NULL) {
+        fprintf(stderr, "Output File cannot open\n");
+        exit(EXIT_FAILURE);
+    }
+    encode_data(fp_output, buffer, data_size);
+    fclose(fp_output);
+
+    return 0;
+}
+
+// 同じデータが並んでいる部分の長さを取得する
+// datas中のx番目の位置を調べ、同じデータ値が並んでいる長さ(ラン長)をrunに、
+// データ値をcodeに返す
+// datasの大きさはdata_sizeで指定する
+// ラン長の最大値はlimitまでに制限する
+// 戻り地は、次に処理対象となるデータの位置
+int get_run_length(byte *datas, int x, int data_size, int limit, int *run,
+                   int *code) {
+    // 処理中のデータ位置
+    int processing_point = x;
+
+    *run = 1;
+    *code = datas[processing_point];
+    processing_point += 1;
+
+    while (processing_point < data_size && *code == datas[processing_point] &&
+           *run < limit) {
+        processing_point += 1;
+        *run += 1;
+    }
+
+    return processing_point;
+}
+
+// ランレングス圧縮して、結果をfpで指定されるファイルに出力する
+void encode_data(FILE *fp, byte *datas, int data_size) {
+    int processing_point = 0;
+    int run, code;
+    while (processing_point < data_size) {
+        processing_point = get_run_length(datas, processing_point, data_size,
+                                          0xff, &run, &code);
+        fputc(run, fp);
+        fputc(code, fp);
+    }
+}
---
+++ CompressSRC/chapter1/list1-7/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned char byte;
+
+int decode_data(FILE *fp_input, FILE *fp_output);
+
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *fp_input = fopen(argv[1], "rb");
+    if (fp_input == NULL) {
+        fprintf(stderr, "Input File cannot open\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 圧縮処理
+    FILE *fp_output = fopen(argv[2], "wb");
+    if (fp_output == NULL) {
+        fprintf(stderr, "Output File cannot open\n");
+        exit(EXIT_FAILURE);
+    }
+    decode_data(fp_input, fp_output);
+    fclose(fp_input);
+    fclose(fp_output);
+}
+
+// fp_inputで指定されているファイルデータをランレングス符号化による圧縮結果とし、
+// それを展開して、結果をfp_outputで指定されるファイルに出力する
+int decode_data(FILE *fp_input, FILE *fp_output) {
+    for (;;) {
+        int length = fgetc(fp_input);
+        if (length == EOF) {
+            break;
+        }
+
+        int code = fgetc(fp_input);
+        if (code == EOF) {
+            // エラー
+            return 0;
+        }
+
+        for (int i = 0; i < length; i++) {
+            fputc(code, fp_output);
+        }
+    }
+
+    return 1;
+}
